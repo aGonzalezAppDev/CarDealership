1738ce11f1db522752d5b3eb930eff64806d585a
aGonzalezAppDev <agonzalez@appdev.yearup.org>
2023-05-12 15:13:39 -0400

    DealershipFileManager update

--- src/main/java/com/learntocode/DealershipFileManager.java
+++ src/main/java/com/learntocode/DealershipFileManager.java
@@ -1,4 +1,31 @@
 package com.learntocode;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class DealershipFileManager {
+    private final ArrayList<Dealership> dealership = new ArrayList<Dealership>();
+
+    public ArrayList<Dealership> getDealership() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("dealership.csv"));
+        String line = "";
+        while ((line = reader.readLine()) != null) {
+            String[] parts = line.split("\\|");
+            if (parts.length == 3) {
+                String name = parts[0];
+                String address = parts[1];
+                String phone = parts[2];
+                dealership.add(new Dealership(name, address, phone));
+            }
+        }
+        return dealership;
+    }
+
+    public static void saveDealership(Dealership dealership){
+
+    }
+
 }
--- src/main/java/com/learntocode/UserInterface.java
+++ src/main/java/com/learntocode/UserInterface.java
@@ -1,4 +1,5 @@
 package com.learntocode;
 
 public class UserInterface {
+    
 }
